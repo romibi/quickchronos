a2a150da58a756df8c1afa0a5f6ec2bd053d87e5
romibi <romibi@bluewin.ch>
2018-05-11 15:54:21 +0200

    Make it nicer

--- public/js/timer-anim.js
+++ public/js/timer-anim.js
@@ -1,7 +1,9 @@
 let radius = 100;
 let starttime = 0;
 
-let bgColor, inColor, sColor, mColor, hColor = null;
+let sColor, mColor, hColor = null;
+let sFadeColor = null;
+let sFadeFinished = true;
 
 function setup() {
 	let canvas = createCanvas(400, 400);
@@ -10,50 +12,65 @@ function setup() {
 	
 	let d = new Date();
 	starttime = d.getTime();
+	starttime-=55000;
 	
-	bgColor = color(255, 255, 255, 10);
-	inColor = color(255, 255, 255, 255);
 	sColor = color(255,0,0,255);
+	sFadeColor = color(255,0,0,0);
+	sFadeTarget = color(255,0,0,0);
+
 	mColor = color(0,255,0,255);
 	hColor = color(0,0,255,255);
 	
 	frameRate(60);
-	noStroke();
-	
+	strokeCap(SQUARE);
+	smooth();
 }
 
 function draw() {
 	translate(width/2, height/2);
 	rotate(radians(-90));
 	
-	fill(bgColor);
-	ellipse(0,0,radius+2);
+	clear();
+
+	noFill();
+	strokeWeight(radius*0.1);
 	
-	let s = getSeconds();
-	let m = floor(s/60);
-	let h = floor(m/60);
+	let seconds = getSeconds();
+	let minutes = floor(seconds/60);
+	let hours = floor(minutes/60);
+
 	
-	
-	fill(sColor);
-	arc(0,0,radius,radius,0,radians(360/60*s));
-	
-	fill(inColor);
-	arc(0,0,radius*0.8, radius*0.8, 0, radians(360/60*(s+1)));
-	
-	if(m>0) {
-		fill(mColor);
-		arc(0,0,radius*0.8, radius*0.8, 0, radians(360/60*m));
+	if(seconds>=60 && seconds%60<5) {
+		if(sFadeFinished && seconds%60<2.5) {
+			sFadeFinished=false;
+			sFadeColor = lerpColor(sColor,sColor,0.0);
+		}
+
+		var oldAlpha = alpha(sFadeColor);
+		sFadeColor = lerpColor(sFadeColor, sFadeTarget, 0.05);
 		
-		fill(inColor);
-		arc(0,0,radius*0.6, radius*0.6, 0, radians(360/60*(m+1)));
+		if(alpha(sFadeColor)==oldAlpha) {
+			sFadeColor.setAlpha(0);
+		} else {
+			stroke(sFadeColor);
+			ellipse(0,0,radius*0.9);
+		}
+	} else {
+		sFadeFinished=true;
+		sFadeColor.setAlpha(0);
+	}
+
+	stroke(sColor);
+	arc(0,0,radius*0.9,radius*0.9,0,radians(360/60*seconds));
+	
+	if(minutes>0) {
+		stroke(mColor);
+		arc(0,0,radius*0.7, radius*0.7, 0, radians(360/60*minutes));
 	}
 	
-	if(h>0) {
-		fill(hColor);
-		arc(0,0,radius*0.6, radius*0.6, 0, radians(360/24*h));
-		
-		fill(inColor);
-		arc(0,0,radius*0.4, radius*0.4, 0, radians(360/24*(h+1)));
+	if(hours>0) {
+		stroke(hColor);
+		arc(0,0,radius*0.5, radius*0.5, 0, radians(360/24*hours));
 	}
 }
 
